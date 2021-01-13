bc7d37744e472eeb9f9f53faeb081493740855af
Bill Beesley <bill.beesley@dazn.com>
2021-01-13 17:17:15 +0000

    feat(install): install to zshrc if present

--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 WORKING_DIR="$(pwd)/bin"
-echo "# git-scripts" >> ~/.bash_profile
-echo "export PATH=\$PATH:${WORKING_DIR}" >> ~/.bash_profile
+if [ -f "~/.bash_profile" ]; then
+    echo "# git-scripts" >> ~/.bash_profile
+    echo "export PATH=\$PATH:${WORKING_DIR}" >> ~/.bash_profile
+fi
+
+if [ -f "~/.zshrc" ]; then
+    echo "# git-scripts" >> ~/.zshrc
+    echo "export PATH=\$PATH:${WORKING_DIR}" >> ~/.zshrc
+fi 
